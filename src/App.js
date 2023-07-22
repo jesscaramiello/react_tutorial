a2e1e19c192b5050d20dc285cfe318f42c8b67c8
Jessica Caramiello <jxc1399@student.bham.ac.uk>
2023-07-22 20:38:29 +0100

    Tutorial finished. 'time travel' implemented and final cleanup completed

--- src/App.js
+++ src/App.js
@@ -63,16 +63,20 @@ function Board({ xIsNext, squares, onPlay }) {
 
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    //setXIsNext(!xIsNext);
   }
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    //setXIsNext(nextMove % 2 === 0);
   }
   const moves = history.map((squares, move) => {
     let description;
@@ -82,7 +86,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
