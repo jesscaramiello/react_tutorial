72023a5fdca204a2d6992883bd95c726c0d840c1
Jessica Caramiello <jxc1399@student.bham.ac.uk>
2023-07-25 22:09:31 +0100

    Extra tutorial question 2: now the board is made by utilising 2 map loops

--- src/App.js
+++ src/App.js
@@ -40,23 +40,29 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+
+  const boardRows = [0,1,2].map((rowIndex) => (
+    <div key={rowIndex} className="board-row">
+    {[0, 1, 2].map((colIndex) => {
+      const index = rowIndex * 3 + colIndex;
+      return (
+        <Square
+          key={index}
+          value={squares[index]}
+          onSquareClick={() => handleClick(index)}
+        />
+      );
+    })}
+  </div>
+));
+
+
   return (<>
-    <div className="status">{status}</div>
-    <div className="board-row">
-      <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
-      <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
-      <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
-    </div>
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
-      <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
-      <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
-      <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
-      <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
-    </div>
+  
+      <div className="status">{status}</div>
+      {boardRows}
+    
+    
   </>);
 }
 
